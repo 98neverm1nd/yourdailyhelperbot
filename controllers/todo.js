634a995cc0ffd2a19da158a24a325a38c2e08600
Ilya <98neverm1nd@gmail.com>
2019-01-06 17:41:54 +0200

    basic version, /add /get /check functions and JSON storage modal

---
+++ controllers/todo.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const Telegram = require('telegram-node-bot');
+
+class TodoController extends Telegram.TelegramBaseController {
+    addHandler($) {
+        let todo = $.message.text.split(' ').slice(1).join(' ');
+
+        if (!todo) return $.sendMessage('Sorry, please pass a todo item.');
+
+        $.getUserSession('todos')
+            .then(todos => {
+                if (!Array.isArray(todos)) $.setUserSession('todos', [todo]);
+                else $.setUserSession('todos', todos.concat([todo]));
+                $.sendMessage('Added new todo!');
+            });
+    }
+    getHandler($) {
+        $.getUserSession('todos').then(todos => {
+            $.sendMessage(this._serializeList(todos), { parse_mode: 'Markdown' });
+        });
+    }
+
+    checkHandler($) {
+        let index = parseInt($.message.text.split(' ').slice(1)[0]);
+        if (isNaN(index-1)) return $.sendMessage('Sorry, you didn\'t pass a valid index.');
+
+        $.getUserSession('todos')
+            .then(todos => {
+                if (index-1 >= todos.length) return $.sendMessage('Sorry, you didn\'t pass a valid index.');
+                todos.splice(index-1, 1);
+                $.setUserSession('todos', todos);
+                $.sendMessage('Checked todo!');
+            });
+    }
+
+    get routes() {
+        return {
+            'addCommand': 'addHandler',
+            'getCommand': 'getHandler',
+            'checkCommand': 'checkHandler'
+        };
+    }
+
+    _serializeList(todoList) {
+        let serialized = '*Your Todos:*\n\n';
+        todoList.forEach((t, i) => {
+            serialized += `*${i+1}* - ${t}\n`;
+        });
+        return serialized;
+    }
+}
+
+module.exports = TodoController;
