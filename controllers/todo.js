acf6e3b09c3a59c2c556eb7dacc66157a7fbb9c2
Ilya <98neverm1nd@gmail.com>
2019-01-07 14:38:03 +0200

    Improved text interface

--- controllers/todo.js
+++ controllers/todo.js
@@ -6,13 +6,13 @@ class TodoController extends Telegram.TelegramBaseController {
     addHandler($) {
         let todo = $.message.text.split(' ').slice(1).join(' ');
 
-        if (!todo) return $.sendMessage('Sorry, please pass a todo item.');
+        if (!todo) return $.sendMessage('*Use /add like this: \'/add My first task.\'*',{ parse_mode: 'Markdown' });
 
         $.getUserSession('todos')
             .then(todos => {
                 if (!Array.isArray(todos)) $.setUserSession('todos', [todo]);
                 else $.setUserSession('todos', todos.concat([todo]));
-                $.sendMessage('Added new todo!');
+                $.sendMessage('*Added new todo!*', { parse_mode: 'Markdown' });
             });
     }
     getHandler($) {
@@ -23,14 +23,14 @@ class TodoController extends Telegram.TelegramBaseController {
 
     checkHandler($) {
         let index = parseInt($.message.text.split(' ').slice(1)[0]);
-        if (isNaN(index-1)) return $.sendMessage('Sorry, you didn\'t pass a valid index.');
+        if (isNaN(index-1)) return $.sendMessage('*Sorry, you didn\'t pass a valid index. For example, removing the first item will be \'/check 1\'*',{ parse_mode: 'Markdown' });
 
         $.getUserSession('todos')
             .then(todos => {
-                if (index-1 >= todos.length) return $.sendMessage('Sorry, you didn\'t pass a valid index.');
+                if (index-1 >= todos.length) return $.sendMessage('*Sorry, you didn\'t pass a valid index.*',{ parse_mode: 'Markdown' });
                 todos.splice(index-1, 1);
                 $.setUserSession('todos', todos);
-                $.sendMessage('Checked todo!');
+                $.sendMessage('*Checked todo!*',{ parse_mode: 'Markdown' });
             });
     }
 
@@ -43,10 +43,13 @@ class TodoController extends Telegram.TelegramBaseController {
     }
 
     _serializeList(todoList) {
-        let serialized = '*Your Todos:*\n\n';
+        let serialized = '*Your Todos*\n\n';
         todoList.forEach((t, i) => {
-            serialized += `*${i+1}* - ${t}\n`;
+            serialized += `*[${i+1}]* - ${t}\n`;
         });
+        if (serialized == '*Your Todos*\n\n') {
+            return '*Your Todolist is currently empty.*';
+        }
         return serialized;
     }
 }
